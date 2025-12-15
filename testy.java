cc8305ddb9127bc7c00c6f8a559e1da16e3e619c
GRALCIO-maker <pioniker3@gmail.com>
2025-12-15 10:08:56 +0100

    Add files via upload

---
+++ testy.java
@@ -0,0 +1,22 @@
+import org.testng.annotations.Test;
+
+public class testy {
+@Test
+    public void test1(){
+    int a=10;
+    int b=20;
+    assert a!=b;
+}
+@Test
+    public void test2() {
+    int a = 10;
+    int b = 20;
+    assert a != b;
+}
+@Test
+    public void test3() {
+        int a = 10;
+        int b = 20;
+        assert a != b;
+    }
+}
